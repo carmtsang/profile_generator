86b19f22456b839391f8b4396b1561ee92274e09
Carmen Tsang <carmtsang89@hotmail.com>
2022-04-23 17:14:02 +0000

    stretched out console.log so answers can be 1 word

--- survey.js
+++ survey.js
@@ -14,7 +14,7 @@ rl.question('What is your Name? Nicknames are also acceptable 😊.   ', (answer
         rl.question('What\'s your favourite thing to eat for that meal?   ', (answer5) => {
           rl.question('Which sport is your absolute favourite?   ', (answer6) => {
             rl.question('What is your superpower? In a few words, tell us what you are amazing at!  ', (answer7) => {
-              console.log(`${answer}. ${answer2}. ${answer3}. ${answer4}. ${answer5}. ${answer6}. ${answer7}.`);
+              console.log(`Hi, my name is ${answer}. I like ${answer2} while listening to ${answer3}. My favourite meal is ${answer4}, I like to eat ${answer5}. My favourite sport is ${answer6} and my superpower is ${answer7}.`);
               rl.close();
             });
           });
